244153de0ad9345b1047afbc26de109703a06c91
Artsiom-Shlapakou <xpressmusik@mail.ru>
2021-04-23 04:09:21 +0300

    fixed bags

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,6 @@
-from rest_framework import viewsets, request, status
-from rest_framework import permissions
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets
 from users.models import User
-from users.serializers import UserSerializer, CreateUserSerializer
-from users.permissions import IsOwnerOrReadOnly
+from users.serializers import UserSerializer
 
 
 # class UserViewSet(viewsets.ModelViewSet):
@@ -20,20 +17,6 @@ from users.permissions import IsOwnerOrReadOnly
 #             return CreateUserSerializer
 #         return UserSerializer
 
-class CreateUserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = CreateUserSerializer
-
-    # POST request
-    @csrf_exempt
-    def create(self, request, *args, **kwargs):
-        serializer = CreateUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'status': 'User created successfully'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def get_serializer_class(self):
-        if self.action == "create":
-            return CreateUserSerializer
-        return UserSerializer
+    serializer_class = UserSerializer
